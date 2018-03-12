5c482614c3b8ac5869732cc9349be663ff34ea82
Ed Me <603803799@qq.com>
2018-03-12 18:50:22 +0800

    modify select-data

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="ux_select" :class="['relative-box', 'cursor-pointer',type&&'active']" @click.stop="type=!type">
-    <p :class="[value&&'select-box']"
-       v-text="value||placeholder"></p>
-    <div class="absolute-box option-box" v-if="type">
+    <p :class="[value&&label&&'select-box']"
+       v-text="value&&label||placeholder"></p>
+    <div class="absolute-box option-box" v-show="type">
       <slot>
         <p style="font-size: 1em;color:red">Select is not slot</p>
       </slot>
@@ -20,6 +20,7 @@
     data () {
       return {
         type: false, // 初始化option展开状态
+        label: '', // select需要展示的字段
         optionEl: document.querySelector('.option-box'), // 存储element元素
       }
     },
@@ -29,17 +30,31 @@
           this.optionEl = this.$el.querySelector('.option-box') // 再一次重置option的DOM
           this.uiUpdate() // 更新用户UI视图
           for (let x of this.optionEl.children) x.onclick = function (event) { // onclick可以覆盖，无需解绑事件
-            return event = event.target, this.$emit('input', this.getValue(event)) // 更新父组件value
+            return event = event.target,
+              this.label = this.getLabel(event) /* 更新父组件展示的数据 */,
+            this.$emit('input', this.getValue(event)) /* 更新父组件value */ &&
+            this.$emit('change', this.getValue(event))
+            /* 触发父组件change事件 */
           }.bind(this)
         })
       },
     },
+    mounted () {
+      this.$nextTick(() => this.init()) // 初始化视图数据
+    },
     methods: {
+      init () { // 首次更新视图
+        this.optionEl = this.$el.querySelector('.option-box') // 获取当前组件的option节点
+        for (let x of this.optionEl.children) this.value == this.getValue(x) && (this.label = this.getLabel(x))
+      },
       getValue (element) { // 获取slot的value
         return element.getAttribute('value')
       },
+      getLabel (element) { // 获取slot的label
+        return element.getAttribute('label')
+      },
       uiUpdate (callback) { // 更新UI视图
-        for (let x of this.optionEl.children) this.value === this.getValue(x)
+        for (let x of this.optionEl.children) this.value == this.getValue(x)
           ? x.classList.add('active')
           : x.classList.remove('active')
         return callback && callback()
